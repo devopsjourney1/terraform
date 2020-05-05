1dbe1d07aebfee2c456b8b6b1f09dfdcd3369f38
bradmorg <bradmorg@gmail.com>
2020-05-04 17:30:17 -0700

    commit to master

---
+++ docker-lab/main.tf
@@ -0,0 +1,15 @@
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
+    keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.latest
+    name = "webserver"
+    ports {
+        internal = 80
+        external = 8050
+    }
+
+
+}
